047a4bef199107f2d824e47e0b03fecc01d9047b
voidapex11 <154328367+voidapex11@users.noreply.github.com>
2025-03-05 08:38:41 +0000

    Quartz sync: Mar 5, 2025, 8:38 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,13 +8,11 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "voidapex11",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
